6f199efe9fd7c1ab2c6f61cce3aca7d477022988
George Kolesnikov <gkolesnikov88@gmail.com>
2020-09-24 10:04:47 +0300

    Create todo list with mock data from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
+// for lazy loading
+// import AddTodo from "./todo/AddTodo";
+import Loader from "./Loader";
+import Modal from "./Modal/Modal";
+
+const AddTodo = React.lazy(() =>
+    new Promise(resolve => {
+        setTimeout(() => {
+            resolve(import('./Todo/AddTodo'))
+        }, 3000)
+    })
+)
 
 function App() {
+
+  const [todos, setTodos] = React.useState([
+    // {id: 1, completed: false, title: 'Buy bread'},
+    // {id: 2, completed: true, title: 'Buy butter'},
+    // {id: 3, completed: false, title: 'Buy milk'},
+  ]);
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+          .then(response => response.json())
+          .then(todos => {
+              setTimeout(() => {
+                  setTodos(todos);
+                  setLoading(false);
+              }, 2000)
+          })
+  }, [])
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+      setTodos(todos.concat([{
+          title: title,
+          id: Date.now(),
+          completed: false
+      }]))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Context.Provider value={{removeTodo: removeTodo}}>
+        <div className='wrapper'>
+          <h1>React tutorial</h1>
+            <Modal></Modal>
+            <React.Suspense fallback={<p>Loading</p>}>
+                <AddTodo onCreate={addTodo}></AddTodo>
+            </React.Suspense>
+
+            {loading && <Loader></Loader>}
+            {todos.length
+                ? <TodoList todos={todos} onToggle={toggleTodo}></TodoList>
+                : loading ? null : <p>No todos</p>
+            }
+        </div>
+      </Context.Provider>
   );
 }
 
